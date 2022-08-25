b86482be492807bad195cb9608739666aeb510db
Slawomir Johansen <baran@fastmail.com>
2022-08-25 22:50:28 +0200

    ADD base API functionality

---
+++ src/repositories/users-mock-repository.ts
@@ -0,0 +1,43 @@
+import { User } from '../interfaces/user.interface';
+import shortid from 'shortid';
+
+export class UsersMockRepository {
+    private users: Array<User> = [];
+    addItem(item: User): User {
+        item.id = shortid.generate();
+        item.role = item.role;
+        this.users.push(item);
+        return item; 
+    }
+    updateItem(id: string, item: User): User {
+        this.users = this.users.map(i => {
+            if (i.id === id) {
+                return {
+                    ...item,
+                    id: i.id,
+                    role: i.role,
+                    updatedAt: new Date()
+                };
+            }
+            return i;
+        });
+        return this.getItemById(id);
+    }
+    deleteItem(id: string): boolean {
+        this.users = this.users.filter(i => i.id !== id);
+        return true;
+    }
+    getItemById(id: string): User {
+        return this.users.find(i => i.id === id);
+    }
+    getAllItems(): Array<User> {
+        return this.users;
+    }
+    getItemByRole(role: string): User {
+        return this.users.find(i => i.role === role);
+    }
+
+    findUserByFirstName(name: string): User {
+        return this.users.find(i => i.firstName === name);
+    }
+}
